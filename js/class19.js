5e1424d8b61ca52d4ee470c0ea90af3c757a55b9
Ajit133 <dasajit697@gmail.com>
2023-11-02 01:26:09 +0600

    number class 19

---
+++ js/class19.js
@@ -0,0 +1,39 @@
+// a number with decimal 
+const a = 10.4;
+console.log(a);
+// a number without decimal 
+const b = 10;
+console.log(b);
+// extra large number
+const extra = 123e5;
+console.log(extra);
+const extrasub = 123e-5;
+console.log(extrasub);
+
+const x = 222222222222222222222;
+console.log(x);
+// Floating Precision
+console.log(0.5 * 10 + 0.1 * 10 ) / 10 ;
+// addition 
+const number1 = 10;
+const number2 = 20;
+console.log(number1 + number2);
+// add two string 
+const num1 = 10;
+const num2 = 20;
+const num3 = "30";
+console.log(num1 + num2 + num3);
+
+const numbr1 = "100";
+const numbr2 = "10";
+console.log(numbr1 / numbr2);
+// numeric number string multiplacation
+const j = NaN;
+const k = "10";
+console.log(j + k);
+console.log(typeof(j));
+
+let myNumber = "35g";
+console.log(myNumber.toString(2));
+console.log(myNumber.toString(8));
+console.log(isNaN(myNumber));
