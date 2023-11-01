e228b138e25f193d655d8cb6b1253932fe87e8b5
Ajit133 <dasajit697@gmail.com>
2023-11-01 23:36:44 +0600

    Template Literals

---
+++ js/clsss18.js
@@ -0,0 +1,12 @@
+// string literale
+const hisname = "Ajit Das Anupam"
+const name = `This is ${hisname}`;
+console.log(name);
+
+const number  = 10;
+console.log(`My Number is ${number}`)
+
+const a = 19;
+const b = 1;
+console.log(`i love Bangladesh .It's population is over ${a + b}`);
+// we write on template literals in expression not like statement 
