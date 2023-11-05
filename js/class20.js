39c895c58db8cda2c283d6105ec74c2be1dc59bb
Ajit133 <dasajit697@gmail.com>
2023-11-06 00:17:27 +0600

    Class 20 Learn Array and Number Method

---
+++ js/class20.js
@@ -0,0 +1,29 @@
+let a = 123;
+let b = a.toString();
+console.log(typeof b);
+let Expo = 124.345;
+let conv = Expo.toExponential(2)
+console.log(typeof conv)
+
+let fix = 9.5534543;
+// let conver = fix.toFixed(2);
+let conver = fix.toFixed()
+console.log(conver);
+console.log(Number("10"));
+console.log(Number("10"));
+console.log(Number(" 10"))
+console.log(Number("John"))
+let convert = parseInt(fix);
+console.log(convert);
+
+const arry = ["Ajit","Rone", 20 ,21 ,23 ,45,34];
+// iteration of array
+const len = arry.length;
+
+for(let i = 0; i<len; i++){
+    console.log(arry[i])
+}
+arry.forEach((item,index)=>{
+     console.log(item)
+     console.log(index)
+})
